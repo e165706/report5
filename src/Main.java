62d1dd20181a57e5aad617669f1ba0df4bcbffbc
Yamato Goto <e165706@ie.u-ryukyu.ac.jp>
2016-12-04 21:49:17 +0900

    report5

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ package  jp.ac.uryukyu.e165706;
 
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        try{ System.out.println(str.length());}
+        String str = "3．14";
+        /*try{ System.out.println(str.length());}
         catch (NullPointerException  e){
             System.out.println("エラー:" + e.getMessage());
             e.printStackTrace();
 
         }
+        */
+        double value= Double.parseDouble(str);
     }
 }
